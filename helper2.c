6496b5fc07bce07eb4ef1f42eee3ee2a6e2c2d8e
Oluwatobi <amure387@gmail.com>
2022-10-17 13:37:40 -0700

    betty helper2

--- helper2.c
+++ helper2.c
@@ -1,5 +1,55 @@
 #include <stdio.h>
 /**
+ * convert - base converter
+ * @a: number to be converted
+ * @base: base to convert to
+ * @flag_upper: takes care of uppercase hex
+ * @flag_ptr: flag pointer
+ * Return: length
+ */
+int convert(unsigned long a, int base, int flag_upper, int flag_ptr)
+{
+	unsigned int num = (unsigned int)a;
+	int i = 0;
+	static char Representation[] = "0123456789ABCDEF";
+	static char sRepresentation[] = "0123456789abcdef";
+	static char buffer[50];
+	char *ptr;
+
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	if (flag_ptr == 1)
+	{
+		do
+		{
+			*--ptr = sRepresentation[a % base];
+			a /= base;
+		} while (a != 0);
+	}
+	else
+	{
+		do
+		{
+			if (flag_upper == 1)
+				*--ptr = Representation[num % base];
+			else
+				*--ptr = sRepresentation[num % base];
+
+			num /= base;
+
+		} while (num != 0);
+	}
+
+	while (ptr[i] != '\0')
+	{
+		_putchar(ptr[i]);
+		i++;
+	}
+
+	return (i);
+}
+/**
  * percent_func - print mod
  * @list: list args
  * Return: 1
--- helper_functions.c
+++ helper_functions.c
@@ -1,53 +1,6 @@
 #include "main.h"
 
 
-
-
-
-
-int convert(unsigned long a, int base, int flag_upper, int flag_ptr)
-{
-	unsigned int num = (unsigned int)a;
-	int i = 0;
-	static char Representation[] = "0123456789ABCDEF";
-	static char sRepresentation[] = "0123456789abcdef";
-	static char buffer[50];
-	char *ptr;
-
-	ptr = &buffer[49];
-	*ptr = '\0';
-
-	if (flag_ptr == 1)
-	{
-		do
-		{
-			*--ptr = sRepresentation[a % base];
-			a /= base;
-		} while (a != 0);
-	}
-	else
-	{
-		do
-		{
-			if (flag_upper == 1)
-				*--ptr = Representation[num % base];
-			else
-				*--ptr = sRepresentation[num % base];
-
-			num /= base;
-
-		} while (num != 0);
-	}
-
-	while (ptr[i] != '\0')
-	{
-		_putchar(ptr[i]);
-		i++;
-	}
-
-	return (i);
-}
-
 int binary_func(va_list list)
 {
 	int b = va_arg(list, unsigned int);
