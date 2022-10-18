d16a09f8bb7cbc5dff6e270e1ee3c8347d421276
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-10-18 14:39:25 +0100

    update

--- helper1.c
+++ helper1.c
@@ -7,18 +7,41 @@
  */
 int digit_func(va_list list)
 {
-	int n = va_arg(list, int);
+	unsigned int m;
+	int i = 0, k = 0, n = 0, count = 0;
 
+	n = va_arg(list, int);
 	if (n <= INT_MAX && n >= INT_MIN)
 	{
-		n = print_int_func(n);
+		if (n < 0)
+		{
+			n *= -1;
+			_putchar('-');
+			count += 1;
+		}
+		m = n;
+		for (k = 0; (m / 10) > 0; k++)
+			m /= 10;
+
+		m = n;
+		while (k != 0)
+		{
+			for (i = 0; i < k; i++)
+				m /= 10;
+			m %= 10;
+			_putchar(m + '0');
+			count++;
+			k--;
+			m = n;
+		}
+		_putchar(m % 10 + '0');
+		count++;
 	}
 	else
 	{
 		return (-1);
 	}
-
-	return (n);
+	return (count);
 }
 /**
  * print_number - prints signed numbers
